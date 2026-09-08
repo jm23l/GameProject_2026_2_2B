7510182df955fdb84e092ca72479eb06339ff8b4
jm23l <sandmin347@gmail.com>
2026-09-08 13:36:49 +0900

    [Feat] Event 기초 테스트 Scene

---
+++ TempProject/Assets/BuildingType.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class BuildingType : MonoBehaviour
+{
+    public enum BuldingType
+    {
+        Restaurant,                 // 음식점           
+        Customer,                   // 고객 집
+        ChargingStation             // 충전소
+    }
+
+}
